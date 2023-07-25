0966a5669d26216b304846c39ec154085579d405
MD Rakib <md.rakib39640@gmail.com>
2023-07-25 16:31:37 +0600

    all css file add now

--- js/cleck.js
+++ js/cleck.js
@@ -1,7 +1,14 @@
 // typing script js
-var typed = new Typed(".typing",{
-    Strings: ["Web Designer","web Developer","ps aexpart"],
+var typed = new Typed(".atuo-typed", {
+    strings: [ "web Designer"," web developer"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true,
+});
+
+var typed = new Typed(".atou_typed-2", {
+    strings: [ "web Designer"," web developer"],
     typeSpeed: 80,
     backSpeed: 80,
 
-});
+})
