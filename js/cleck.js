f4395e9cf71cac698955ada30c87aede9e953811
MD Rakib <md.rakib39640@gmail.com>
2023-07-26 12:50:27 +0600

    add ja file

--- js/cleck.js
+++ js/cleck.js
@@ -11,4 +11,21 @@ var typed = new Typed(".atou_typed-2", {
     typeSpeed: 80,
     backSpeed: 80,
 
-})
+});
+// hide faqs ansewer
+const faqs = document.querySelectorAll('.faq');
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+            // cheng icon
+            const icon = faq.querySelector('.faq_icon i');
+            if(icon.className === 'fa-solid fa-plus'){
+                icon.className = 'fa-solid fa-minus';
+            }
+            else{
+                icon.className = 'fa-solid fa-plus';
+            }
+        })
+
+});
+
